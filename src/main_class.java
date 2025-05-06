5f6d469a1d9846f09166d98b76b28ac2e26a7241
muhammadhasnainmughal <muhammadhasnainmughal.bscsf23@iba-suk.edu.pk>
2025-05-06 10:01:49 +0500

    init

--- src/main_class.java
+++
@@ -1,8 +0,0 @@
-public class main_class {
-    public static void main(String[] args) throws InterruptedException {
-        choice r=new choice();
-        //login l = new login();
-        r.setVisible(true);
-       
-    }
-}
